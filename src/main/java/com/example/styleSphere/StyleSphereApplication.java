642c526e4f8a4b26018c951ed321db533c14eba0
chan <jhgu128@naver.com>
2025-03-09 16:13:18 +0900

    first commit

---
+++ src/main/java/com/example/styleSphere/StyleSphereApplication.java
@@ -0,0 +1,13 @@
+package com.example.styleSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/styleSphere/StyleSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.styleSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
